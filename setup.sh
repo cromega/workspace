85e3422f1dc8bd20bc736c28ea34dd73be55bf27
Bence Monus <crome@moronia.hu>
2015-04-28 21:47:19 +0100

    I really don't want more logic in bash

--- setup.sh
+++ setup.sh
@@ -1,21 +1,31 @@
-#!/bin/bash -e
+#!/bin/bash
 
-stupid_error() {
+stupid_message() {
     echo ********************** $1 **************************
 }
 
 die_if_no_git() {
   if [ ! git ]; then
-    stupid_error "OMG NO GIT"
+    stupid_message "THIS SYSTEM'S LACK OF GIT IS DISTURBING"
     exit 1
   fi
 }
 
-[ ! -L ~/.vimrc ] && ln -s .vim/.vimrc ~/.vimrc
+[ ! vim ] && stupid_message "VIM IS NOT INSTALLED, WHAT THE ACTUAL FUCK"
+if [ ! -L ~/.vimrc -a ! -f ~/.vimrc -a ! -d ~/.vim ]; then
+  echo "vim stuff is sub-par, fixing..."
+  cp -r .vim ~ && ln -s ~/.vim/.vimrc ~/.vimrc
+  echo "vim is sorta fixed"
+fi
 
-for file in .tmux.conf .gitconfig; do [ ! -f $file ] && cp $file ~; done
+for file in .tmux.conf .gitconfig; do
+  if [ ! -f $file ]; then
+    cp $file ~
+    echo "placed $file where it belongs"
+  fi
+done
 
-[ ! zsh ] && echo ********************** INSTALL ZSH! *************************
+[ ! zsh ] && stupid_message "ZSH EATH BASH FOR BREAKFAST, GO GET SOME"
 if [ ! -d ~/.oh-my-zsh ]; then
   wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
   echo 'got zsh now!'
