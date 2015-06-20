49ac6a314b6e2cd095a68dc0e6528cf6dfe1754c
Bence Monus <crome@moronia.hu>
2015-06-20 21:21:17 +0100

    stop this knocking nonsense

--- files/crap.zsh
+++ files/crap.zsh
@@ -9,11 +9,4 @@ export EDITOR=/usr/bin/vim
 export LC_ALL=en_US.utf8
 export TERM=xterm-256color
 
-k() {
-  if [ -z "$1" ]; then
-    echo "host needed"
-    return 1
-  fi
 
-  knock $1 $(read '?ports: '; echo -n "$REPLY")
-}
