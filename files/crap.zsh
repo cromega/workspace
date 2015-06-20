fe3393b7cbcd01b42992b014e5f4d154cfcd73e9
Bence Monus <crome@moronia.hu>
2015-06-20 21:21:33 +0100

    set up the terminal properly

--- files/crap.zsh
+++ files/crap.zsh
@@ -7,6 +7,10 @@ alias be="bundle exec"
 
 export EDITOR=/usr/bin/vim
 export LC_ALL=en_US.utf8
-export TERM=xterm-256color
 
+if [[ -n $TMUX ]]; then
+  export TERM="screen-256color"
+else
+  export TERM="xterm-256color"
+fi
 
