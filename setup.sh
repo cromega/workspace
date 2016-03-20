9fc690fde7abe889ea439ba2f0ff66cd1445bef8
Bence Monus <crome@moronia.hu>
2016-03-20 16:34:02 +0000

    just die on error

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 [ ! -z $DEBUG ] && set -x
 
@@ -63,12 +63,10 @@ go_version="1.5.3"
 os=$(uname -s | tr 'A-Z' 'a-z')
 mkdir -p ~/go
 if [ ! -d ~/go/$go_version ]; then
-  set -e
   wget https://storage.googleapis.com/golang/go${go_version}.${os}-amd64.tar.gz -O - | tar -xzf - -C ~/go
   mv ~/go/go ~/go/$go_version
   [ -L ~/go/current ] && rm ~/go/current
   ln -s ~/go/$go_version ~/go/current
-  set +e
 fi
 file go.zsh ~/.oh-my-zsh/custom/
 
