f4111259aa8e666b16b46bf5692c7934ae499e07
Bence Monus <bence.monus@contractor.isbanuk.com>
2017-03-06 12:30:15 +0000

    easier install files

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -5,6 +5,10 @@ if [ ! -d ~/.oh-my-zsh ]; then
   wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 fi
 
-put crap.zsh ~/.oh-my-zsh/custom/
-put docker.zsh ~/.oh-my-zsh/custom/
-put pivotal.zsh ~/.oh-my-zsh/custom/
+if [ ! -x /usr/bin/zsh ]; then
+  echo "*** WARN: zsh is not installed"
+fi
+
+for file in files/*; do
+  put "$file" ~/.oh-my-zsh/custom
+done
