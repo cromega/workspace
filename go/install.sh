645808c03bdc5ee4ace321676d5dafa2b1ecc5d0
Bence Monus <bence.monus@contractor.isbanuk.com>
2017-03-06 11:24:14 +0000

    link runtime if version is already downloaded

--- go/install.sh
+++ go/install.sh
@@ -3,12 +3,16 @@ cd $(dirname $0)
 
 go_version="1.8"
 os=$(uname -s | tr 'A-Z' 'a-z')
+
 mkdir -p ~/go
 if [ ! -d ~/go/$go_version ]; then
   wget https://storage.googleapis.com/golang/go${go_version}.${os}-amd64.tar.gz -O - | tar -xzf - -C ~/go
   mv ~/go/go ~/go/$go_version
-  [ -L ~/go/current ] && rm ~/go/current
-  ln -s ~/go/$go_version ~/go/current
 fi
+
+echo "Linking Go ${go_version}"
+[ -L ~/go/current ] && rm ~/go/current
+ln -s ~/go/$go_version ~/go/current
+
 put go.zsh ~/.oh-my-zsh/custom/
 
