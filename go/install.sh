9b7f092e17b961b4c3a9c8406059c0c4f3841df2
Bence Monus <crome@moronia.hu>
2017-03-08 16:48:12 +0000

    prefer curlies

--- go/install.sh
+++ go/install.sh
@@ -6,13 +6,13 @@ os=$(uname -s | tr 'A-Z' 'a-z')
 
 mkdir -p ~/go
 if [ ! -d ~/go/$go_version ]; then
-  wget https://storage.googleapis.com/golang/go${go_version}.${os}-amd64.tar.gz -O - | tar -xzf - -C ~/go
-  mv ~/go/go ~/go/$go_version
+  mkdir ~/go/${go_version}
+  wget https://storage.googleapis.com/golang/go${go_version}.${os}-amd64.tar.gz -O - | tar -xzf - -C ~/go/${go_version}
 fi
 
 echo "Linking Go ${go_version}"
-[ -L ~/go/current ] && rm ~/go/current
-ln -s ~/go/$go_version ~/go/current
+rm -f ~/go/current
+ln -s ~/go/${go_version} ~/go/current
 
 put go.zsh ~/.oh-my-zsh/custom/
 
