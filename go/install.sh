b1548d3e8005f6970d1e0aaef2645cc04b8a5145
Bence Monus <crome@moronia.hu>
2017-03-09 22:51:37 +0000

    extract into the right dir

--- go/install.sh
+++ go/install.sh
@@ -7,7 +7,7 @@ os=$(uname -s | tr 'A-Z' 'a-z')
 mkdir -p ~/go
 if [ ! -d ~/go/$go_version ]; then
   mkdir ~/go/${go_version}
-  wget https://storage.googleapis.com/golang/go${go_version}.${os}-amd64.tar.gz -O - | tar -xzf - -C ~/go/${go_version}
+  wget https://storage.googleapis.com/golang/go${go_version}.${os}-amd64.tar.gz -O - | tar -xzf - -C ~/go/${go_version} --strip-components=1
 fi
 
 echo "Linking Go ${go_version}"
