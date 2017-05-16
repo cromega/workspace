4bc01c352ef5abb6de7abfd578c946b36222d0a9
Bence Monus <crome@moronia.hu>
2017-05-16 15:58:34 +0100

    fix make flag, duh.

--- rbenv/rbenv.zsh
+++ rbenv/rbenv.zsh
@@ -1,6 +1,6 @@
 export PATH="${HOME}/.rbenv/bin:$PATH"
 #export RUBY_BUILD_CACHE_PATH='.rbenv/download_cache'
 mkdir -p $RUBY_BUILD_CACHE_PATH
-export MAKE_OPTS=$(($(grep -c MHz /proc/cpuinfo) + 1))
+export MAKE_OPTS="-j$(($(grep -c MHz /proc/cpuinfo) + 1))"
 
 eval "$(rbenv init -)"
