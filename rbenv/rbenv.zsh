8a580692285a42304338e0cd90edecf980152968
Bence Monus <crome@moronia.hu>
2017-05-16 15:58:21 +0100

    fails for some reason, fix later

--- rbenv/rbenv.zsh
+++ rbenv/rbenv.zsh
@@ -1,5 +1,5 @@
 export PATH="${HOME}/.rbenv/bin:$PATH"
-export RUBY_BUILD_CACHE_PATH='.rbenv/download_cache'
+#export RUBY_BUILD_CACHE_PATH='.rbenv/download_cache'
 mkdir -p $RUBY_BUILD_CACHE_PATH
 export MAKE_OPTS=$(($(grep -c MHz /proc/cpuinfo) + 1))
 
