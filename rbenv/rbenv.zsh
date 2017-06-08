6b863906676f00230f9685c72a6989aedfd40d37
Bence Monus <crome@moronia.hu>
2017-06-08 22:14:30 +0100

    stop zsh startup echoing an error

--- rbenv/rbenv.zsh
+++ rbenv/rbenv.zsh
@@ -1,6 +1,7 @@
 export PATH="${HOME}/.rbenv/bin:$PATH"
 #export RUBY_BUILD_CACHE_PATH='.rbenv/download_cache'
-mkdir -p $RUBY_BUILD_CACHE_PATH
+#mkdir -p $RUBY_BUILD_CACHE_PATH
+
 export MAKE_OPTS="-j$(($(grep -c MHz /proc/cpuinfo) + 1))"
 
 eval "$(rbenv init -)"
