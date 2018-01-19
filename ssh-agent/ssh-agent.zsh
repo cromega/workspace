4a93dbd33dfbff3de48a186f3ae8413372513cc1
Bence Monus <crome@moronia.hu>
2018-01-19 17:45:59 +0000

    start an agent if not running

--- ssh-agent/ssh-agent.zsh
+++ ssh-agent/ssh-agent.zsh
@@ -3,4 +3,6 @@ pid=$(pidof ssh-agent)
 if [ $? -eq 0 ]; then
   export SSH_AGENT_PID=$pid
   export SSH_AUTH_SOCK="$(find /tmp/ -iname 'ssh-*' -type s -user $USER -print -quit)"
+else
+  eval $(ssh-agent) > /dev/null
 fi
