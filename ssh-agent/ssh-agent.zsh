5eb07cc3d6aab3b3258c7ae0841e16e3f1a6bfb9
Bence Monus <crome@moronia.hu>
2018-01-19 17:45:33 +0000

    prevent non-existent glob from causing an error

--- ssh-agent/ssh-agent.zsh
+++ ssh-agent/ssh-agent.zsh
@@ -2,5 +2,5 @@ pid=$(pidof ssh-agent)
 
 if [ $? -eq 0 ]; then
   export SSH_AGENT_PID=$pid
-  export SSH_AUTH_SOCK="$(find /tmp/ssh-* -type s -user $USER -print -quit)"
+  export SSH_AUTH_SOCK="$(find /tmp/ -iname 'ssh-*' -type s -user $USER -print -quit)"
 fi
