a826b41d280c255e794e77e1887432b934b38dc0
Bence Monus <crome@moronia.hu>
2018-01-26 16:38:48 +0000

    fix ssh agent script
    the ssh agent creates a directory, not a socket

--- ssh-agent/ssh-agent.zsh
+++ ssh-agent/ssh-agent.zsh
@@ -2,7 +2,7 @@ pid=$(pidof ssh-agent)
 
 if [ $? -eq 0 ]; then
   export SSH_AGENT_PID=$pid
-  export SSH_AUTH_SOCK="$(find /tmp/ -iname 'ssh-*' -type s -user $USER -print -quit)"
+  export SSH_AUTH_SOCK="$(find /tmp/ -iname 'ssh-*' -type d -user $USER -print -quit)"
 else
   eval $(ssh-agent) > /dev/null
 fi
