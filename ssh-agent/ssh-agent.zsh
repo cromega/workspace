45ab7a22411e525f0feef82dd9f7ff5d1278b005
Bence Monus <crome@moronia.hu>
2018-01-30 23:05:32 +0000

    fix ssh agent script some more

--- ssh-agent/ssh-agent.zsh
+++ ssh-agent/ssh-agent.zsh
@@ -4,7 +4,7 @@ pid=$(pidof ssh-agent)
 
 if [ $? -eq 0 ]; then
   export SSH_AGENT_PID=$pid
-  export SSH_AUTH_SOCK="$(find /tmp/ -iname 'ssh-*' -type d -user $USER -print -quit)"
+  export SSH_AUTH_SOCK="$(find /tmp/ -type s -iname 'agent*' -user $USER | head -n 1)"
 else
   eval $(ssh-agent) > /dev/null
 fi
