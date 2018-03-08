fa7784488c3687dd999b31ea6d3964d758cccade
Bence Monus <crome@moronia.hu>
2018-03-08 22:52:05 +0000

    add module for installing singularity

---
+++ singularity/install.sh
@@ -0,0 +1,25 @@
+cd $(dirname $0)
+. ../lib.sh
+
+tmpdir="$HOME/tmp/singularity"
+
+cleanup() {
+  echo "cleaning up..."
+  rm -rf $tmpdir
+}
+
+trap "cleanup" EXIT
+
+sudo apt-get install automake libtool python squash-tools
+
+github singularityware/singularity $tmpdir
+cd $tmpdir
+
+git checkout $(git tag -l | tail -n 1)
+./autogen.sh
+./configure --prefix=/usr/local
+
+cpus=$(grep -c processor /proc/cpuinfo)
+make -j $cpus
+
+sudo make install
