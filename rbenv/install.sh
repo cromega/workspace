e7b289562f24b7af069cfcf22fecc98f5e9d640e
Bence Monus <crome@moronia.hu>
2018-08-24 18:41:03 +0100

    install ruby build deps

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -7,3 +7,7 @@ github sstephenson/ruby-build ~/.rbenv/plugins/ruby-build
 put rbenv.zsh ~/.oh-my-zsh/custom/
 put .gemrc ~
 
+if check_command "apt-get"; then
+  echo install the following packages:
+  echo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+fi
