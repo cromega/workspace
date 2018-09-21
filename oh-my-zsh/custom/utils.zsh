a148484e7f6a07c183167ba21a0fc33f0278990e
Bence Monus <crome@moronia.hu>
2018-09-21 16:10:53 +0100

    not quite working bin stubbing method
    the stub can't export into the PATH because it runs in a
    different shell. not quite useful

--- oh-my-zsh/custom/utils.zsh
+++ oh-my-zsh/custom/utils.zsh
@@ -1,3 +1,19 @@
 stubonce() {
   eval "$1() { unfunction $1; $2; $1 \$@ }"
 }
+
+STUBS_DIR=~/bin/stubs
+mkdir -p $STUBS_DIR
+if ! [[ $PATH =~ $STUBS_DIR ]]; then
+  export PATH=$PATH:$STUBS_DIR
+fi
+
+bin_stub_once() {
+  stub=$1
+  target=$2
+  caller=$3
+
+  echo -e "#!/usr/bin/zsh\nsource $caller\n$target\nrm \$0\n$stub \$@" > $STUBS_DIR/$stub
+  chmod +x $STUBS_DIR/$stub
+}
+
