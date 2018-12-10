a5eaa51f24e531c63d7cbebbec792000f87dc29e
Bence Monus <crome@moronia.hu>
2018-12-10 11:08:29 +0000

    install missing packages

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -8,6 +8,5 @@ put rbenv.zsh ~/.oh-my-zsh/custom/
 put .gemrc ~
 
 if check_command "apt-get"; then
-  echo install the following packages:
-  echo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+  sudo apt-get -y install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
 fi
