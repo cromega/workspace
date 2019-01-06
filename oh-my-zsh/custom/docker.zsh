67c4f27862bc6a7bd5ca25cb825713a57c5fc1a1
Bence Monus <crome@moronia.hu>
2019-01-06 12:49:34 +0000

    use shorter alias

--- oh-my-zsh/custom/docker.zsh
+++ oh-my-zsh/custom/docker.zsh
@@ -2,6 +2,6 @@ list_docker_ips() {
   docker ps | awk '{print $1}' | while read id; do echo "$id: $(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $id)"; done
 }
 
-alias doc="docker-compose"
+alias dc="docker-compose"
 alias alpine="docker run --rm -it alpine:latest ash -c \"apk update; ash\""
 alias ubuntu="docker run --rm -it ubuntu:latest bash -c \"apt-get update; bash\""
