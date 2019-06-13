b6e3e1d459076b9cb7f4c33ba7bc7886faa600f4
Bence Monus <crome@moronia.hu>
2019-06-13 11:45:33 +0100

    pin version

--- g/install.sh
+++ g/install.sh
@@ -1,7 +1,9 @@
 cd $(dirname $0)
 . ../lib.sh
 
-curl https://raw.githubusercontent.com/stefanmaric/g/master/bin/g > ~/bin/g
+G_VERSION=0.4.0
+
+curl https://raw.githubusercontent.com/stefanmaric/g/$G_VERSION/bin/g >~/bin/g
 chmod +x ~/bin/g
 
 put g.zsh ~/.oh-my-zsh/custom/
