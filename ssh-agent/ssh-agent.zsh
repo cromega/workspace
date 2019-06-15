0d8ad930802b9e35ecd4bed5a66fec04f181298c
Bence Monus <crome@moronia.hu>
2019-06-15 15:55:17 +0100

    some directories may be forbidden to list

--- ssh-agent/ssh-agent.zsh
+++ ssh-agent/ssh-agent.zsh
@@ -4,7 +4,7 @@ pid=$(pidof ssh-agent)
 
 if [ $? -eq 0 ]; then
   export SSH_AGENT_PID=$pid
-  export SSH_AUTH_SOCK="$(find /tmp/ -type s -iname 'agent*' -user $USER | head -n 1)"
+  export SSH_AUTH_SOCK="$(find /tmp/ssh-* -type s -iname 'agent*' -user $USER | head -n 1)"
 else
   eval $(ssh-agent) > /dev/null
 fi
