b1621e130549e0f3026e54b82243cf385cff4610
Bence Monus <crome@moronia.hu>
2019-11-15 01:04:57 +0000

    this turned out to be a difficult to manage idea

--- rbenv/install.sh
+++ rbenv/install.sh
@@ -1,16 +1,8 @@
 cd $(dirname $0)
 . ../lib.sh
 
-DEPENDENCIES="autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev"
-
 github sstephenson/rbenv ~/.rbenv
 github sstephenson/ruby-build ~/.rbenv/plugins/ruby-build
 
 put rbenv.zsh ~/.oh-my-zsh/custom/
 put gemrc ~/.gemrc
-
-if check_command "apt-get"; then
-  if ! dpkg -l $DEPENDENCIES > /dev/null; then
-    sudo apt-get -y install $DEPENDENCIES
-  fi
-fi
