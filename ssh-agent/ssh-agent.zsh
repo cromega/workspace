ab7e7afdeb343d293bf31ca5fd0c2194f19c0acd
Bence Monus <crome@moronia.hu>
2020-08-03 14:57:00 +0200

    try to fix the ssh agent wrapper once and for all

--- ssh-agent/ssh-agent.zsh
+++ ssh-agent/ssh-agent.zsh
@@ -1,10 +1,22 @@
-[ -S /var/run/user/$UID/keyring/ssh ] && return
+# if something manages an agent, exit
+[ -S /var/run/user/$UID/keyring/ssh ] && return # Cinnamon
+
+start_new_agent() {
+  find /tmp -type d -iname 'ssh*' -exec rm -rf {} \;
+  eval $(ssh-agent) > /dev/null
+}
 
 pid=$(pidof ssh-agent)
-
-if [ $? -eq 0 ]; then
-  export SSH_AGENT_PID=$pid
-  export SSH_AUTH_SOCK="$(find /tmp/ssh-* -type s -iname 'agent*' -user $USER | head -n 1)"
-else
-  eval $(ssh-agent) > /dev/null
+if [ -z $pid ]; then
+  start_new_agent
+  return
 fi
+
+socket=$(find /tmp -type s | grep "ssh.*agent.$(($pid - 1))")
+if [ -z $socket ]; then
+  start_new_agent
+  return
+fi
+
+export SSH_AGENT_PID=$pid
+export SSH_AUTH_SOCK=$socket
