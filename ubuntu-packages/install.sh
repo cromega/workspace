516ee0a41acf6f4e2d8d12a77835a9053fc6a369
Bence Monus <crome@moronia.hu>
2021-08-18 18:13:18 +0200

    try to install everything in one go

---
+++ ubuntu-packages/install.sh
@@ -0,0 +1,22 @@
+packages="zsh
+git
+vim
+build-essential
+htop
+atop
+powertop
+terminator
+tmux
+wget
+curl
+jq
+silversearcher-ag
+openvpn
+tree
+tig
+cmus
+libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev
+libpq5 libpq-dev libsqlite3-0 libsqlite3-dev
+"
+
+sudo apt-get install $packages
